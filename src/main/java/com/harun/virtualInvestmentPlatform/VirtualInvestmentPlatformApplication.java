8059fd2810fd18f041577a0b83f8dd63f4994917
Harun Eren Özkaya <harunerenozkaya@gmail.com>
2023-12-15 14:23:58 +0300

    init

---
+++ src/main/java/com/harun/virtualInvestmentPlatform/VirtualInvestmentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.harun.virtualInvestmentPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualInvestmentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualInvestmentPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harun/virtualInvestmentPlatform/VirtualInvestmentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harun.virtualInvestmentPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualInvestmentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
